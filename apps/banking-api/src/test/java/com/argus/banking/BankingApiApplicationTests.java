fd1e3b788b1b67daa2470e24b18ed349fb51cdf7
unknown <semih.ozyurek@student.uhasselt.be>
2026-09-22 00:49:12 +0200

    Add banking API starter project

---
+++ apps/banking-api/src/test/java/com/argus/banking/BankingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argus.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
